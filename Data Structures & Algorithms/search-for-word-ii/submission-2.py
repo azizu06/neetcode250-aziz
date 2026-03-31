fd1f7a69df30183c65d8761705be2aeaf035b3f4
Abduaziz Umarov <ab725492@ucf.edu>
2026-03-31 19:35:39 -0400

    Add: search-for-word-ii - submission-2

---
+++ Data Structures & Algorithms/search-for-word-ii/submission-2.py
@@ -0,0 +1,39 @@
+class Trie:
+    def __init__(self):
+        self.children = {}
+        self.end = False
+        self.found = False
+        self.word = ""
+
+class Solution:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        root = Trie()
+        for w in words:
+            cur = root
+            for c in w:
+                if c not in cur.children: cur.children[c] = Trie()
+                cur = cur.children[c]
+            cur.end = True
+            cur.word = w
+        def dfs(cur, r, c):
+            if cur.end and not cur.found:
+                res.append(cur.word)
+                cur.found = True
+            temp = board[r][c]
+            board[r][c] = '.'
+            steps = [[-1, 0], [1, 0], [0, 1], [0, -1]]
+            for y, x in steps:
+                nr, nc = r+y, c+x
+                if nr < 0 or nr >= rows or nc < 0 or nc >= cols: continue
+                if board[nr][nc] not in cur.children: continue
+                dfs(cur.children[board[nr][nc]], nr, nc)
+            board[r][c] = temp
+
+        res = []
+        rows, cols = len(board), len(board[0])
+        for r in range(rows):
+            for c in range(cols):
+                if board[r][c] in root.children:
+                    cur = root.children[board[r][c]]
+                    dfs(cur, r, c)
+        return res
