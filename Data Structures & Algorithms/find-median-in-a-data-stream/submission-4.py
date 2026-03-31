dc7b4e46a036c8485e831b354d11e40ec0d856f3
Abduaziz Umarov <ab725492@ucf.edu>
2026-03-31 15:00:05 -0400

    Add: find-median-in-a-data-stream - submission-4

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-4.py
@@ -0,0 +1,25 @@
+class MedianFinder:
+
+    def __init__(self):
+        self.maxH = []
+        self.minH = []
+
+    def addNum(self, num: int) -> None:
+        if self.maxH and num < -self.maxH[0]:
+            heapq.heappush(self.maxH, -num)
+        else:
+            heapq.heappush(self.minH, num)
+        minLen, maxLen = len(self.minH), len(self.maxH)
+        if minLen-maxLen >= 2:
+            heapq.heappush(self.maxH, -heapq.heappop(self.minH))
+        elif maxLen-minLen >= 2:
+            heapq.heappush(self.minH, -(heapq.heappop(self.maxH)))
+        
+    def findMedian(self) -> float:
+        minLen, maxLen = len(self.minH), len(self.maxH)
+        if minLen > maxLen: return self.minH[0]
+        if maxLen > minLen: return -self.maxH[0]
+        return (-self.maxH[0]+self.minH[0])/2.0
+
+        
+        
