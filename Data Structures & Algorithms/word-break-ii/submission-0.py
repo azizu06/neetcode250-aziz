26f7136884cf9978a85677154c9b3a5ebfb0951b
Abduaziz Umarov <ab725492@ucf.edu>
2026-04-02 02:27:07 -0400

    Add: word-break-ii - submission-0

---
+++ Data Structures & Algorithms/word-break-ii/submission-0.py
@@ -0,0 +1,30 @@
+class Trie:
+    def __init__(self):
+        self.children = {}
+        self.word = ""
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        root = Trie()
+        for w in wordDict:
+            cur = root
+            for c in w:
+                if c not in cur.children: cur.children[c] = Trie()
+                cur = cur.children[c]
+            cur.word = w
+        res, path = [], []
+        def dfs(i, root):
+            if i == len(s):
+                res.append(" ".join(path[:]))
+                return
+            cur = root
+            for j in range(i, len(s)):
+                if s[j] not in cur.children: break
+                cur = cur.children[s[j]]
+                if cur.word:
+                    path.append(cur.word)
+                    dfs(j+1, root)
+                    path.pop()
+                
+                
+        dfs(0, root)
+        return res
