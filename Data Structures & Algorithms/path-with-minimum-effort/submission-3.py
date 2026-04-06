b3474f0812e5ac3964816c8d59602f67a10014a0
Abduaziz Umarov <ab725492@ucf.edu>
2026-04-06 10:39:43 -0400

    Add: path-with-minimum-effort - submission-3

---
+++ Data Structures & Algorithms/path-with-minimum-effort/submission-3.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minimumEffortPath(self, heights: List[List[int]]) -> int:
+        R,C = len(heights), len(heights[0])
+        heap = [[0,0,0]]
+        visit = set()
+        steps = [(1,0),(-1,0),(0,1),(0,-1)]
+        while heap:
+            best,r,c = heapq.heappop(heap)
+            if (r,c) in visit: continue
+            visit.add((r,c))
+            if r == R-1 and c == C-1: return best
+            for y,x in steps:
+                nr,nc = r+y,c+x
+                if min(nr,nc) < 0 or nr >= R or nc >= C: continue
+                diff = abs(heights[r][c]-heights[nr][nc])
+                newBest = max(diff,best)
+                heapq.heappush(heap,(newBest,nr,nc))
+
