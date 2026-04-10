796f9ce3109effa8e1b4f6c72c9071818b669530
Abduaziz Umarov <ab725492@ucf.edu>
2026-04-10 01:13:29 -0400

    Add: coin-change-ii - submission-0

---
+++ Data Structures & Algorithms/coin-change-ii/submission-0.py
@@ -0,0 +1,9 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        dp = [0]*(amount+1)
+        dp[0] = 1
+        for c in coins:
+            for i in range(amount+1):
+                if i+c > amount: continue
+                dp[i+c]+=dp[i]
+        return dp[amount]
