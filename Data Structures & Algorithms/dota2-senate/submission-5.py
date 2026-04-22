1d68d89ef9c762d8dd4415a1b946ffe3fd1aa7df
Abduaziz Umarov <ab725492@ucf.edu>
2026-04-21 21:16:15 -0400

    Add: dota2-senate - submission-5

---
+++ Data Structures & Algorithms/dota2-senate/submission-5.py
@@ -0,0 +1,24 @@
+from collections import deque
+
+class Solution:
+    def predictPartyVictory(self, senate: str) -> str:
+        r = deque()
+        d = deque()
+        n = len(senate)
+
+        for i, ch in enumerate(senate):
+            if ch == 'R':
+                r.append(i)
+            else:
+                d.append(i)
+
+        while r and d:
+            ri = r.popleft()
+            di = d.popleft()
+
+            if ri < di:
+                r.append(ri + n)
+            else:
+                d.append(di + n)
+
+        return "Radiant" if r else "Dire"
